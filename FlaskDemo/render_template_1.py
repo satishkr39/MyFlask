ee54792ecf921305fa19baadd83dc06e31b3895d
satishkr39 <satishkr63@gmail.com>
2021-06-28 14:29:11 +0530

    adding control statments

--- FlaskDemo/render_template_1.py
+++ FlaskDemo/render_template_1.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
 
+# Passing the variable from Python to HTML template using {{my_variable}}
 @app.route('/')
 def home_page():
-    return render_template('home.html')
-    # return "Hello World"
+    my_var = 'Satish'  # Passing a single word
+    my_list = ['Apple', 'Bat', 'Cat']  # Passing a list
+    my_dict = {1: 'Satish', 2:'Kumar', 3: 'Singh'}  # Passing Dictionary
+    return render_template('home.html', var_name=my_var, my_list = my_list, my_dict=my_dict)
 
 
 @app.route('/about')
